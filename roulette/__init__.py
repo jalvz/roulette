25ddd5756fed923cb09d7ea58486bfb982cd12d1
jalvz <ihavetheblues@gmail.com>
2015-04-13 22:31:54 +0200

    full implementation

--- roulette/__init__.py
+++ roulette/__init__.py
@@ -1,30 +1,81 @@
 """
-Library to execute tests repeatedly and define success criteria.
+Roulette is a library to execute flaky tests repeatedly, allowing to define arbitrary success
+conditions.
 """
 import functools
+import time
+import sys
 
 
-def roll(times, delay=0, success=0.66, until_success=True, only=[AssertionError], except_=None):
+def roll(at_most, at_least=1, success=1, delay=0, only=None, except_=None):
     """
-    :param times (int): Maximum number of times to execute the test.
-    :param delay (int): Number of seconds to wait between repetitions.
-    :param success: Criteria which defines a successful test. If `success` is an <int> i, the test
-    will be considered successful when i repetitions are successful.
+    :param at_most (int): Maximum number of times to execute the test.
+
+    :param at_least (int): Minimum number of times to execute the test.
+    [DEFAULT = 1]
+
+    :param success (int|float): Threshold which defines a successful test.
+    If `success` is an <int> i, the test will be marked as successful when i repetitions are
+    successful.
     If `success` is a <float> f between 0.0 and 1.0, the test will be considered successful when
     the number of successful repetitions / number of total repetitions is >= f.
-    :param until_success (bool): If true, it will stop executing the test when success condition
-    is met.
-    :param only (list): Whitelist of exceptions that can throw the test which will cause repeated
-    executions according to `times` and `until_success` arguments.
-    If `except_` is specified, only must be None.
-    :param except_ (list): Blacklist of exceptions that can throw the test which will mark test as
-    failed regardless any other settings.
-    If `only` is specified, expect_ must be None.
+
+    The test must run `at_least` times before it can be considered successful or `at_most` times
+    before it can be considered failed.
+    [DEFAULT = 1]
+
+    :param delay (int): Number of seconds to wait between repetitions.
+    [DEFAULT = 0]
+
+    :param only (list): Whitelist of exceptions which, if raised by the test, won't mark the test
+    as failed. Any exception not whitelisted will cause the test to fail immediately regardless
+    the `at_least` settings
+    [DEFAULT = ASSERTION_ERROR]
+
+    :param except_ (list): Blacklist of exceptions which, if raised by the test, will mark the test
+    as failed regardless the `at_least` settings. Any exception not blacklisted will be swallowed
+    and the test might be repeated according to the `at_most` settings.
+    [DEFAULT = None]
+
+    `only` and `expect_` are mutually exclusive, only one can be supplied.
     """
-    def decorator(func):
+
+    def decorate(func):
+        assert at_most >= at_least, "at_most must be greater or equal than at_least"
+        assert not (only and except_), "only and except_ are mutually exclusive"
+        assert isinstance(success, int) or (isinstance(success, float) and 0.0 <= success <= 1.0), \
+            "success must be an int or a float between 0.0 and 1.0"
+
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            for _ in range(times):
-                func(*args, **kwargs)
+            ok, last_e = 0, None
+            for n in range(1, at_most + 1):
+                if n >= at_least + 1 and _is_successful(success, ok, n):
+                    return
+
+                if n > 1:
+                    time.sleep(delay)
+
+                try:
+                    func(*args, **kwargs)
+                    ok += 1
+
+                except Exception, e:
+                    blacklisted = except_ and type(e) in except_
+                    not_whitelisted = not except_ and type(e) not in (only or [AssertionError])
+                    if blacklisted or not_whitelisted:
+                        raise
+                    last_e = e
+
+            if not _is_successful(success, ok, n):
+                raise last_e, None, sys.exc_info()[-1]
+
         return wrapper
-    return decorator
+
+    return decorate
+
+
+def _is_successful(success, ok, n):
+    absolute_success = isinstance(success, int) and ok >= success
+    relative_success = isinstance(success, float) and ok / float(n) >= success
+    return absolute_success or relative_success
--- test/TestRoll.py
+++ test/TestRoll.py
@@ -1,14 +1,103 @@
-import random
+import functools
 from unittest2 import TestCase
 from roulette import roll
 
 
+def _fail_assert():
+    assert 1 == 0
+
+
+def _fail_attribute():
+    raise AttributeError
+
+
+def fibo_gen(n=1, m=1):
+    while True:
+        yield n
+        aux = n + m
+        n = m
+        m = aux
+
+
+def meta_test(stop_at=None, exc_type=None):
+    def decorate(func):
+        @functools.wraps(func)
+        def wrapper(this, *args, **kwargs):
+            try:
+                func(this, *args, **kwargs)
+                assert exc_type is None, "Exception not raised {}".format(exc_type)
+            except Exception, e:
+                assert isinstance(e, exc_type), "Unexpected exception raised {}".format(type(e))
+
+            nx = this.next()
+            assert stop_at == nx, \
+                "Next element in sequence does not match {} != {}".format(stop_at, nx)
+        return wrapper
+    return decorate
+
+
 class TestRoll(TestCase):
 
-    @roll(10)
-    def test_sum(self):
-        self.assertEqual(1 + 2, 3)
+    def setUp(self):
+        self.fibo = fibo_gen()
+        self.next = self.fibo.next
+        # 1, 1, 2, 3, 5, 8, 13, 21, 34...
 
-    @roll(10)
-    def test_rand(self):
-        self.assertGreater(random.randint(0, 20), 2)
+    @meta_test(1)
+    @roll(3, success=1)
+    def test_simple(self):
+        self.assertGreater(self.next(), 0)
+
+    @meta_test(13)
+    @roll(8, success=3)
+    def test_success_int(self):
+        self.assertGreater(self.next(), 2)
+
+    @meta_test(5, AssertionError)
+    @roll(4, success=1)
+    def test_fail_int(self):
+        self.assertEqual(self.next(), 5)
+
+    @meta_test(34)
+    @roll(8, success=0.5)
+    def test_success_float(self):
+        self.assertGreater(self.next(), 4)
+
+    @meta_test(34, AssertionError)
+    @roll(8, success=0.51)
+    def test_fail_float(self):
+        self.assertGreater(self.next(), 4)
+
+    @meta_test(13)
+    @roll(8, success=3)
+    def test_success_int(self):
+        self.assertGreater(self.next(), 2)
+
+    @meta_test(3)
+    @roll(3, success=1, only=[AttributeError, AssertionError])
+    def test_only_pass(self):
+        if self.next() == 1:
+            raise AttributeError
+
+    @meta_test(1, AttributeError)
+    @roll(3, success=1)
+    def test_only_fail(self):
+        if self.next() == 1:
+            raise AttributeError
+
+    @meta_test(3)
+    @roll(3, success=1, except_=[KeyError])
+    def test_except_pass(self):
+        if self.next() == 1:
+            raise AttributeError
+
+    @meta_test(1, AttributeError)
+    @roll(3, success=1, except_=[AttributeError])
+    def test_except_fail(self):
+        if self.next() == 1:
+            raise AttributeError
+
+    @meta_test(8)
+    @roll(10, success=1, at_least=5)
+    def test_at_least(self):
+        self.assertGreater(self.next(), 0)
